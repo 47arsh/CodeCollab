29e15cf9f1a2ae6fe523f6a030c0045ebf946367
47arsh <arxhx7@gmail.com>
2026-06-07 22:26:45 +0530

    Prepare backend for deployment

--- server/server.js
+++ server/server.js
@@ -14,14 +14,14 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
 
 const userSocketMap = {};
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("CodeCollab Server Running");

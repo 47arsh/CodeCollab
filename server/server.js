87a19f899ba1090180a01951db1cfca6d9af96e4
47arsh <arxhx7@gmail.com>
2026-06-07 14:53:19 +0530

    integrate Socket.IO server with Express backend

--- server/server.js
+++ server/server.js
@@ -1,6 +1,19 @@
 const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
 
 const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
 
 const PORT = 5000;
 
@@ -8,6 +21,6 @@ app.get("/", (req, res) => {
   res.send("CodeCollab Server Running");
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
